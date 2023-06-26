4d4dad423081622962b74fb4ffe0fa94d84d9934
Megami <megami.ay@gmail.com>
2023-06-26 16:36:27 -0300

    dd

--- src/screens/AddTask.jsx
+++ src/screens/AddTask.jsx
@@ -1,3 +1,18 @@
-export default function AddTask(){
-    
+import { Paragraph, TextInput } from "react-native-paper";
+import { Text, View } from "react-native";
+
+export default function AddTask() {
+    return (
+        <View>
+            <Text>Adicionar Tarefa</Text>
+            <Paragraph>Nome/Título</Paragraph>
+            <TextInput
+                placeholder="Insira o Nome"
+            />
+            <Paragraph>Descrição</Paragraph>
+            <TextInput
+                placeholder="Insira a Descrição"
+            />
+        </View>
+    )
 }
--- src/screens/EditTask.jsx
+++ src/screens/EditTask.jsx
@@ -1,3 +1,18 @@
-export default function EditTask(){
-    
+import { Paragraph, TextInput } from "react-native-paper";
+import { View, Text } from "react-native";
+
+export default function EditTask() {
+    return (
+        <View>
+            <Text>Editar Tarefa</Text>
+            <Paragraph>Editar o nome</Paragraph>
+            <TextInput
+                placeholder="Insira o novo nome"
+            />
+            <Paragraph>Editar a descrição</Paragraph>
+            <TextInput
+                placeholder="Insira a nova descrição"
+            />
+        </View>
+    )
 }
