30de1fc23cfef3bd5a8e6fad5a155787cc623aad
Megami <megami.ay@gmail.com>
2023-06-28 13:52:53 -0300

    add, erro a ser corrigido

--- src/screens/AddTask.jsx
+++ src/screens/AddTask.jsx
@@ -1,18 +1,51 @@
-import { Paragraph, TextInput } from "react-native-paper";
+import { Button, Paragraph, TextInput } from "react-native-paper";
 import { Text, View } from "react-native";
+import db from "react-native-vector-icons/FontAwesome";
+import { useState } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 
 export default function AddTask() {
+    const [taskName, setTaskName] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+
+    const handleAddTask = async () => {
+        try {
+            const taskRef = await db.collection("tasks").add({
+                name: taskName,
+                description: taskDescription,
+            });
+            console.log("Task added with ID:", taskRef.id);
+            setTaskName("");
+            setTaskDescription("");
+        } catch (error) {
+            console.error("Error adding task:", error);
+        }
+    };
+
     return (
         <View>
             <Text>Adicionar Tarefa</Text>
             <Paragraph>Nome/Título</Paragraph>
             <TextInput
                 placeholder="Insira o Nome"
+                value={taskName}
+                onChangeText={text => setTaskName(text)}
             />
             <Paragraph>Descrição</Paragraph>
             <TextInput
                 placeholder="Insira a Descrição"
+                value={taskDescription}
+                onChangeText={text => setTaskDescription(text)}
             />
+            <Button
+                icon={() => <Icon name="plus" size={30} color="#000" />}
+                mode="contained"
+                onPress={handleAddTask}
+            >
+                ADICIONAR
+            </Button>
+
         </View>
     )
 }
